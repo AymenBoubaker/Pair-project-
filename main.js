31c634b8e4cd1f37bec985d0138bbcdb08732543
karimmaaroufB <karim-maarouf@hotmail.com>
2023-01-24 11:22:03 +0100

    version finale

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+// el function values kool tamlelhom clear :
+function clearScreen() {
+    $("#result").val("") 
+}
+
+
+// el function dhedhy ta3ml display lel values
+function display(value) {
+    $("#result").val($("#result").val()+value) 
+}
+
+    
+
+// el function hedhy te7sbelna result ml inputed value  : 
+function calculate() {
+    var p = $("#result").val();
+// eval t7wel string lel number !!!!!!****
+    var q = eval(p);
+   $("#result").val(q) 
+}
